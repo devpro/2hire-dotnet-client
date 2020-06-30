66f678194345a8f1ce1e87800a56e68ee9385e7a
Bertrand THOMAS <bertrand@devpro.fr>
2020-06-30 08:34:52 +0200

    Improve test debug environment value names

--- test/Twohire.Client.IntegrationTests/Sandbox/Sandbox2hireRestApiConfiguration.cs
+++ test/Twohire.Client.IntegrationTests/Sandbox/Sandbox2hireRestApiConfiguration.cs
@@ -4,15 +4,15 @@ namespace Devpro.Twohire.Client.IntegrationTests.Sandbox
 {
     public class Sandbox2hireRestApiConfiguration : ITwohireClientConfiguration
     {
-        public string BaseUrl => Environment.GetEnvironmentVariable("TwoHire_Sandbox_BaseUrl");
+        public string BaseUrl => Environment.GetEnvironmentVariable("TwoHire__Sandbox__BaseUrl");
 
-        public string Version => Environment.GetEnvironmentVariable("TwoHire_Sandbox_ApiVersion");
+        public string Version => Environment.GetEnvironmentVariable("TwoHire__Sandbox__ApiVersion");
 
-        public string ServiceToken => Environment.GetEnvironmentVariable("TwoHire_Sandbox_ServiceToken");
+        public string ServiceToken => Environment.GetEnvironmentVariable("TwoHire__Sandbox__ServiceToken");
 
-        public string Username => Environment.GetEnvironmentVariable("TwoHire_Sandbox_Username");
+        public string Username => Environment.GetEnvironmentVariable("TwoHire__Sandbox__Username");
 
-        public string Password => Environment.GetEnvironmentVariable("TwoHire_Sandbox_Password");
+        public string Password => Environment.GetEnvironmentVariable("TwoHire__Sandbox__Password");
 
         public string HttpClientName => "TwoHire";
     }
